ae4693a2f0d8214358edabcd0948dad926e318dd
georg.krautinger <k4KEo9q7>
2022-01-23 23:58:12 +0100

    output should work, still one binding error

--- View/ListView.xaml.cs
+++ View/ListView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ef_excercise
+namespace ef_excercise.View
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaktionslogik für ListView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ListView : Window
     {
-        public MainWindow()
+        public ListView()
         {
             InitializeComponent();
         }
--- ViewModel/EfViewModel.cs
+++ ViewModel/EfViewModel.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ef_excercise.Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using ef_excercise.Context;
+using System.Windows.Input;
+using DownloadManager.ViewModel;
 
 namespace ef_excercise.ViewModel
 {
-    class EfViewModel
+    class EfViewModel : INotifyPropertyChanged
     {
+        public InvoiceContext InvoiceCTX { get; set; }
+        public EfViewModel()
+        {
+            CreateCommand = new RelayCommand(e =>
+            {
+                addUser();
+            }, c => true);
+
+
+
+        }
+        public ICommand CreateCommand { get; private set; }
 
         public List<Invoice> invoice
         {
@@ -22,12 +36,37 @@ namespace ef_excercise.ViewModel
 
             }
         }
-                public EfViewModel()
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] String propertyName = "")
         {
-            using (var ctx = new InvoiceContext())
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public void addUser()
+        {
+            using (InvoiceCTX = new InvoiceContext())
             {
-              
+                Invoice inv = new Invoice()
+                {
+                    CustomerName = "Mike",
+                    CustomerAddress = "King's Street 1",
+                    Amount = 10000.0,
+                    Vat = 10,
+                    InvoiceDate = DateTime.Now
+                };
+
+                try
+                {
+                    Console.WriteLine(inv.Id);
+                    InvoiceCTX.Invoices.Add(inv);
+                    InvoiceCTX.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
             }
         }
-}
+    }
 }
